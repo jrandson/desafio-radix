827426f7274ddc6f218f6d9f23dc57519e9aa34a
J Cunha <jrandson@gmail.com>
2017-05-20 16:11:48 -0300

    Principal analysis components

--- desafio_radix.py
+++ desafio_radix.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib
 from matplotlib import pyplot as plt
+from sklearn.decomposition import RandomizedPCA
 matplotlib.style.use('ggplot')
 
 def get_outliers_index(df, columns, gama = 1.5):
@@ -10,10 +11,7 @@ def get_outliers_index(df, columns, gama = 1.5):
         q2 = df[column].median()
         q3 = df[df[column] > q2][column].median()
         q1 = df[df[column] < q2][column].median()
-
         IQR = q3 - q1
-
-        print len(index_to_drop)
         index_to_drop += list(df[(df[column] > q3 + gama*IQR) | (df[column] < q1 - gama*IQR)][column].index.values)
 
     return list(np.unique(index_to_drop))
@@ -24,18 +22,14 @@ p1_data_train_df = pd.read_csv('p1_data_train.csv',header=0)
 
 #removing the outliers
 print "Removing outliers..."
-gama = 3
+gama = 1.5
 index_to_drop = get_outliers_index(p1_data_train_df,['Temp1','Temp2','Temp3','Temp4'], gama = gama)
 p1_data_train_df = p1_data_train_df.drop(p1_data_train_df.index[index_to_drop])
 
 
 #getting de data traing, using 30% of the values
-pct_train = 0.9
-count_train = int(len(p1_data_train_df)*pct_train)
-count_test = int((1 - pct_train)*len(p1_data_train_df))
-
-#data_train_df = p1_data_train_df[p1_data_train_df.index <= count_train]
-data_train_df = p1_data_train_df.sample(frac=.3)
+pct_train = 1
+data_train_df = p1_data_train_df.sample(frac= pct_train)
 
 data_train = data_train_df[['Temp1','Temp2', 'Temp3', 'Temp4']].values
 target = data_train_df[['target']].values
@@ -44,62 +38,42 @@ target = np.reshape(target,(c,l))[0]
 
 #training the models
 from sklearn.preprocessing import StandardScaler
-
 from sklearn import linear_model
-from sklearn import svm
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.naive_bayes import GaussianNB
-from sklearn.neural_network import MLPClassifier
+classifyer = linear_model.LogisticRegression(C=1, max_iter = 5000, solver = 'lbfgs')
 
-#classifyer = linear_model.LogisticRegression(C=1, max_iter = 1000, solver = 'lbfgs') #solver = 'sag, lbfgs'
+#extracting the most important features from the dataset
+n_components = 4
+pca = RandomizedPCA(n_components = n_components, whiten=True).fit(data_train)
+data_train_pca = pca.transform(data_train)
 
-#kernel = linea, poly, rbf
-#, degree = 3
-classifyer = svm.SVC(kernel = 'rbf',C = 1e2, gamma='auto', max_iter = -1)
-
-#classifyer = DecisionTreeClassifier(min_samples_split = 50, random_state=0)
-
-#classifyer = GaussianNB()
-
-#classifyer = MLPClassifier(solver='lbfgs', alpha = 1e-5,hidden_layer_sizes = (4), random_state=1)
+for item in zip(pca.explained_variance_,pca.explained_variance_ratio_):
+    print item
 
 print "training the model..."
 scaler = StandardScaler()
-data_train = scaler.fit_transform(data_train)
-classifyer.fit(data_train, target)
+data_train = scaler.fit_transform(data_train_pca)
 
+classifyer.fit(data_train, target)
 #predicting the test values for all values of the train test
-#data_test_df = p1_data_train_df[p1_data_train_df.index > count_test]
 data_test_df = p1_data_train_df
 data_test = data_test_df[['Temp1','Temp2', 'Temp3','Temp4']].values
+data_test_pca = pca.transform(data_test)
 target_test = data_test_df[['target']].values
 
 print "predicting the values..."
-data_test = scaler.fit_transform(data_test)
-predicted_values = classifyer.predict(data_test)
-print np.unique(predicted_values)
 
-#getting accuracy
-rate = 0.0
-for i in range(len(predicted_values)):
-    if target_test[i] == predicted_values[i]:
-        rate+= 1
+data_test = scaler.fit_transform(data_test_pca)
+predicted_values = classifyer.predict(data_test_pca)
+print "unique predicted values:", np.unique(predicted_values)
 
-accuracy = 1.0*rate / len(predicted_values)
-print "Accuracy:", accuracy
-#print "Score:", logreg.score(data_test, target_test)
+print "Log reg score:", classifyer.score(data_test, target_test)
 
-#Accuracy logist regression: 0.892877711653
-#Accuracy SVM: 0.896717220196
-#Accuracy decision tree: 0.88769437512
-#Accuracy decision Naive Bayes: 0.895373392206
 
 print "prediting values from the data_test sample"
-index_to_drop = get_outliers_index(p1_data_test_df,['Temp1','Temp2', 'Temp3','Temp4'], gama = gama)
-
-print len(index_to_drop)
+index_to_drop = []#get_outliers_index(p1_data_test_df,['Temp1','Temp2', 'Temp3','Temp4'], gama = gama)
 
 p1_data_test_df = p1_data_test_df.drop(p1_data_test_df.index[index_to_drop])
+print "%d itens droped out" % len(index_to_drop)
 
 data_test = p1_data_test_df[['Temp1','Temp2', 'Temp3','Temp4']].values
 
@@ -107,9 +81,9 @@ predicted_test_values = classifyer.predict(data_test)
 print len(predicted_test_values)
 print np.unique(predicted_test_values)
 
+print "Classifications:"
 print "1:", len(predicted_test_values[predicted_test_values == 1])
 print "0:", len(predicted_test_values[predicted_test_values == 0])
-
 
 del p1_data_test_df
 del p1_data_train_df
